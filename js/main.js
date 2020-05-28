b4c9d0ed4503ccb23a00ae6dc236225cb3cd42a6
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-28 13:03:00 +0200

    Format

--- js/main.js
+++ js/main.js
@@ -34,100 +34,100 @@ function updateStats() {
     unit = "m/s";
   }
 
-  $.get(
-    `https://raw.githubusercontent.com/filiptronicek/czech-weather/master/data/${city}/${today}.csv`,
-    function (data) {
-      const lddPoints = getDataPointsFromCSV(data, metric);
-      let xs = [];
-      let ys = [];
-      for (let i of lddPoints) {
-        xs.push(i.x);
-        ys.push(i.y);
-      }
-      console.log("Formatted outputs: ");
-      const filteredy = ys.filter(function (value, index, ar) {
-        return (index % 2 == 0);
-      });
-      const filteredx = xs.filter(function (value, index, ar) {
-        return (index % 2 == 0);
-      });
+  const url =
+    "https://raw.githubusercontent.com/filiptronicek/czech-weather/master/data/${city}/${today}.csv";
 
-      const options = {
-        series: [
-          {
-            name: lbl,
-            data: ys,
-          },
-        ],
-        responsive: [
-          {
-            breakpoint: 1000,
-            options: {
-              dataLabels: {
-                 enabled: false
+  $.get(url, function (data) {
+    const lddPoints = getDataPointsFromCSV(data, metric);
+    let xs = [];
+    let ys = [];
+    for (let i of lddPoints) {
+      xs.push(i.x);
+      ys.push(i.y);
+    }
+    console.log("Formatted outputs: ");
+    const filteredy = ys.filter(function (value, index, ar) {
+      return index % 2 == 0;
+    });
+    const filteredx = xs.filter(function (value, index, ar) {
+      return index % 2 == 0;
+    });
+
+    const options = {
+      series: [
+        {
+          name: lbl,
+          data: ys,
+        },
+      ],
+      responsive: [
+        {
+          breakpoint: 1000,
+          options: {
+            dataLabels: {
+              enabled: false,
+            },
+            series: [
+              {
+                data: filteredy,
+                labels: lbl,
               },
-              series: [
-                {
-                  data: filteredy,
-                  labels: lbl
-                },
-              ],
-                xaxis: {
-                  categories: filteredx,
-                  title: {
-                    text: "Time",
-                  }
-                  },
-              legend: {
-                position: "bottom"
-              }
-            }
-          }
-        ],
-        chart: {
-          height: 350,
-          type: "line",
-          zoom: {
-            type: "x",
-            enabled: true,
-            autoScaleYaxis: true,
-          },
-        },
-        colors: [getRandomColor()],
-        dataLabels: {
-          enabled: true,
-        },
-        stroke: {
-          curve: "smooth",
-        },
-        title: {
-          align: "center",
-        },
-        grid: {
-          row: {
-            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-            opacity: 0.5,
-          },
-        },
-        yaxis: {
-          labels: {
-            formatter: function (val) {
-              return Number.parseFloat(val).toFixed(2) + unit;
+            ],
+            xaxis: {
+              categories: filteredx,
+              title: {
+                text: "Time",
+              },
+            },
+            legend: {
+              position: "bottom",
             },
           },
         },
-        xaxis: {
-          categories: xs,
-          title: {
-            text: "Time",
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          type: "x",
+          enabled: true,
+          autoScaleYaxis: true,
+        },
+      },
+      colors: [getRandomColor()],
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      title: {
+        align: "center",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      yaxis: {
+        labels: {
+          formatter: function (val) {
+            return Number.parseFloat(val).toFixed(2) + unit;
           },
         },
-      };
+      },
+      xaxis: {
+        categories: xs,
+        title: {
+          text: "Time",
+        },
+      },
+    };
 
-      const chart = new ApexCharts(chartDiv, options);
-      chart.render();
-    }
-  );
+    const chart = new ApexCharts(chartDiv, options);
+    chart.render();
+  });
 }
 
 function getDataPointsFromCSV(csv, metric) {
@@ -145,7 +145,11 @@ function getDataPointsFromCSV(csv, metric) {
           x: parseFloat(points[0]) + offset + ":00",
           y: parseFloat(points[metric]),
         });
-      } else if (i % 2 === 0 && parseFloat(points[0]) + offset < 25 && csvLines.length < 24) {
+      } else if (
+        i % 2 === 0 &&
+        parseFloat(points[0]) + offset < 25 &&
+        csvLines.length < 24
+      ) {
         dataPoints.push({
           x: parseFloat(points[0]) + offset + ":30",
           y: parseFloat(points[metric]),
